190ec06e28cf6b050a2bcd7d967f452f24389ed9
lilian-2021 <lilian.kigunda@student.moringaschool.com>
2022-03-05 17:56:09 +0300

    install django crispy-forms

--- users/views.py
+++ users/views.py
@@ -2,16 +2,16 @@ from django.shortcuts import render,redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from django.contrib.auth import logout,login,authenticate
-
+from .forms import UserRegisterForm
 def registration (request):
     if request.method=='POST':
-        form = UserCreationForm(request.POST)
+        form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
             messages.success(request,f'Account created for {username}!')
             return redirect ('home')
     else:
-        form =UserCreationForm()
-    form=UserCreationForm()
+        form =UserRegisterForm()
+    form=UserRegisterForm()
     return render (request, 'users/register.html',context={'form':form})
