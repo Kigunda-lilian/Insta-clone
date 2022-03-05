e5f7e96b7be4a56fef815777af66179d56054ad7
lilian-2021 <lilian.kigunda@student.moringaschool.com>
2022-03-06 00:01:45 +0300

    import settings and static in insta project url.py file

--- insta/urls.py
+++ insta/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import re_path as url,include
 from django.conf import settings
+from django.conf.urls.static import static
 from users import views as user_views
 
 
