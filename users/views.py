cb5553fe341f1962574a0ec1f6562c9d13855c59
lilian-2021 <lilian.kigunda@student.moringaschool.com>
2022-03-06 12:58:22 +0300

    create userupdate form and profile update form. import them in views.py

--- users/views.py
+++ users/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from django.contrib.auth import logout,login,authenticate
 from django.contrib.auth.decorators import login_required
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm,UserUpdateForm,ProfileUpdateForm
 
 
 def registration (request):
