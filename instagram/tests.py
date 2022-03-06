c0546c3bfb93f2c2520d5a4966f2c41b0c344169
lilian-2021 <lilian.kigunda@student.moringaschool.com>
2022-03-06 15:26:02 +0300

    create an image and test

--- instagram/tests.py
+++ instagram/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
+from .models import *
 
 # Create your tests here.
+
+class ImageTestCase(TestCase):
+    def setUp(self):
+        """image creation
+        """
+        user = User.objects.create(
+            username = 'nashlil',
+            first_name = 'lilian',
+            last_name = 'kanana')
+        
+        Image.objects.create(
+            name="me",
+            caption="ooops",
+            profile_id=user.id,
+            user_id=user.id
+        )
+    def test_image_name(self):
+        """tests image name
+        """
+        image=Image.objects.get(name="me")
+        self.assertEqual(image.name, "me")
