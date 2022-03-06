8f2370e65e9ed3683ca0bb1c026ce44e5aa2ffd5
lilian-2021 <lilian.kigunda@student.moringaschool.com>
2022-03-06 15:32:22 +0300

    test image id

--- instagram/tests.py
+++ instagram/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from .models import *
+from users.models import Profile
 
 # Create your tests here.
 
@@ -23,3 +24,38 @@ class ImageTestCase(TestCase):
         """
         image=Image.objects.get(name="me")
         self.assertEqual(image.name, "me")
+
+
+class LikeTestCase(TestCase):
+    def setUp(self):
+         user = User.objects.create(
+            username = 'Lucy',
+            first_name = 'Mutanu',
+            last_name = 'Kioko')
+         
+         Profile.objects.create(
+            bio = 'me',
+            profile_photo = 'media/profile_pics/pizzacart10-removebg-preview_edhHkdQ.png',
+            user_id = user.id
+        )
+        
+         Image.objects.create(
+            name="me",
+            caption="ooops",
+            profile_id=user.id,
+            user_id=user.id
+        )
+
+
+def test_image_id(self):
+         user = User.objects.create(
+            username = 'nashlil',
+            first_name = 'lilian',
+            last_name = 'kanana')
+         
+         Image.objects.create(
+            name="me",
+            caption="ooops",
+            profile_id=user.id,
+            user_id=user.id
+        )
