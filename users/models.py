d399e867a8c3bb57aa14ef0a13bdfa32b035b5f2
lilian-2021 <lilian.kigunda@student.moringaschool.com>
2022-03-06 14:36:44 +0300

    add save, update and delete methods in profile model

--- users/models.py
+++ users/models.py
@@ -6,6 +6,17 @@ from PIL import Image
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+    bio = models.TextField(max_length=650,blank=True,null=True)
+    
+    def save_profile(self):
+        self.save()
+        
+    def delete_profile(self):
+        self.save()
+        
+    def update(self):
+        self.save()
+    
 
     def __str__(self):
         return f'{self.user.username} Profile'
@@ -15,7 +26,7 @@ class Profile(models.Model):
 
         img = Image.open(self.image.path)
 
-        if img.height > 300 or img.width > 300:
-            output_size = (300, 300)
+        if img.height > 280 or img.width > 280:
+            output_size = (280, 280)
             img.thumbnail(output_size)
             img.save(self.image.path)
